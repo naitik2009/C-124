4af57a97a8413a67869aa43ade987df9353522d0
naitik2009 <89187483+naitik2009@users.noreply.github.com>
2021-12-16 19:21:31 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+ function setup()
+ {
+     video = createCapture(VIDEO);
+     video.size(550, 500);
+
+     canvas = createCanvas(550, 550);
+     canvas.position(560, 150);
+
+     poseNet = ml5.poseNet(video, modelLoaded);
+     posenet.on('pose', gotPoses);
+ }
+
+ function modelLoaded()
+ {
+     console.log('Posenet is Initialized!');
+ }
+
+ function draw ()
+ {
+     background('#00FF00');
+ }
+
+ function gotPoses(results)
+ {
+     if(results.length > 0)
+     {
+         console.log(results);
+     }
+ }
